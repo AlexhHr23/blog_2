ec5c7afe02aff9a5d1aa360914ea1b09c1d63126
Alex Hernandez Rios <84199764+AlexhHr23@users.noreply.github.com>
2024-06-28 11:51:54 -0600

    Implementación de imagenes y que sea reactivo

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Post, PostImage
 
-admin.site.register(Post)
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1  # Número de formularios de imágenes vacíos para mostrar
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageInline]
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,17 @@ import datetime
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    image = models.ImageField()
     description = models.TextField()
+    date = models.DateField(default=datetime.date.today)
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='blog/images')
-    date = models.DateField(datetime.date.today)
+
+    def __str__(self):
+        return f"Image for {self.post.title}"
